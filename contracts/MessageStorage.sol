a4eb75783959ab53a747e9ea6ce96d6bb4b797a3
Aditya Dhir <31381639+adi44@users.noreply.github.com>
2024-06-27 19:30:55 +0530

    Add files via upload

---
+++ contracts/MessageStorage.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+contract MessageStorage {
+    string public message;
+    address public owner;
+
+    event Withdrawal(uint amount, uint when);
+
+    constructor(string memory __message) payable {
+        owner = msg.sender;
+        message = __message;
+    }
+
+    function setMessage(string calldata __message) external {
+        if (owner != msg.sender) {
+            revert (
+                "msg.sender is not owner"
+            );
+        }
+        message = __message;
+    }
+}
